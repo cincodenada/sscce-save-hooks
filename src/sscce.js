c88e0956920f6f25d5592410f03ad80a4e7d15fa
Joel Bradshaw <joel@vivor.com>
2021-12-15 12:58:54 -0800

    Add big ol' comment about what this is

--- src/sscce.js
+++ src/sscce.js
@@ -24,6 +24,29 @@ module.exports = async function() {
     }
   });
 
+  /* This example demonstrate a few approaches to persisting calculated fields
+   * with Sequelize. The approaches are as follows, each calculated field is
+   * calculated differently:
+   *
+   * - sum: Calculate in beforeSave()
+   *   This is probably the simplest, but the calculated field will not be
+   *   available until after save() is called
+   *
+   * - exponent: Calculate in getter, set in beforeSave()
+   *   This is probably the cleanest option - the value will always be
+   *   available via the getter, and it's only set once in the beforeSave()
+   *
+   * - product: Calculate in a getter, set it in the setters of the fields it depends on 
+   *   This is a little duplicative, but still ensures the calculated field
+   *   will always be set internally as soon as its dependents are set. There
+   *   may be situations where this makes more sense than getter + hooks, but I
+   *   don't know of any.
+   *
+   * - dividend: Calculate only in setters of fields it depends on
+   *   This is not recommended because it's very duplicative, it's just here to
+   *   demonstrate that the field will always be set even without a getter
+   */
+
   const Pair = sequelize.define('Pair', {
     a: {
       type: DataTypes.INTEGER,
@@ -50,30 +73,36 @@ module.exports = async function() {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 0,
-      get() {
-        return this.a * this.b
-      }
+      get() { return this.a * this.b }
     },
     dividend: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 0
+    },
+    exponent: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      get() { return Math.pow(this.a,this.b) }
     }
   });
   // Sum - using hooks
   Pair.beforeSave(function(instance, options) {
     instance.set('sum', instance.a+instance.b)
+    instance.set('exponent', instance.exponent)
+    // This is equivalent to the above, and it works, but looks even weirder (and makes some linting mad)
+    // instance.exponent = instance.exponent
   })
 
   await sequelize.sync();
 
   const pair = Pair.build({ a: 3, b: 2 });
-  console.log('Sum before save:', pair.sum);
-  console.log('Product before save:', pair.product);
-  console.log('Dividend before save:', pair.dividend);
+  console.log('Before save', pair.get());
   await pair.save()
   const result = await Pair.findOne();
   expect(result.sum).to.equal(5);
   expect(result.product).to.equal(6);
   expect(result.dividend).to.equal(1.5);
+  expect(result.exponent).to.equal(9);
 };
